3bf3c65cc29078de8c75ddf9bb393445579359e9
Tom White <tom@sixdozen.com>
2013-08-09 02:17:32 -0700

    skeletor: lein new defmain

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to defmain
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/defmain/core_test.clj
@@ -0,0 +1,7 @@
+(ns defmain.core-test
+  (:use clojure.test
+        defmain.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
